0506cb4850cfe5385897845681e634e4b4c54966
ikitty <xkeitty@163.com>
2014-06-20 22:35:51 +0800

    update

--- tgpagination.js
+++ tgpagination.js
@@ -1,40 +1,44 @@
 /**
- * mod pager
+ * TG pager
  *
- * @param {Object} o ,necessary property: wrapId, totalCount, perPage, render
+ * @param {Object} o ,necessary property: pagerWrap, totalCount, perPage, render
  **/
-TJ.pager = function (o) {
+TGPager = function (o) {
     for (var i in o) {
         this[i] = o[i];
     }
     this.pageNum = Math.ceil(this.totalCount / this.perPage);
-
-    var wrap = document.getElementById(this.wrapId);
-    this.dom = {
-        btnPrev: wrap.getElementsByTagName('div')[0]
-        ,btnNext: wrap.getElementsByTagName('div')[1]
-        ,btn: wrap.getElementsByTagName('div')
-        ,num: wrap.getElementsByTagName('p')
-    };
-    this.dom.lastNum = this.dom.num[0];
     this.init();
 };
-TJ.pager.prototype = {
+TGPager.prototype = {
     init: function () {
         this.render(0);
         this.createPage();
         this.numClick();
         this.btnClick();
-        //todo auto create num ele
     }
 
     ,createPage: function () {
-        var sHtml = '<div class="">上一页</div>'; 
-        for (var i = 1; i <= len; i++) {
-            sHtml += '<p>' + i + '</p>';
+        var wrap = this.pagerWrap,
+            sHtml = '<div tgpType="btnPrev" class="tgp_disabled">上一页</div>';
+
+        for (var i = 1; i <= this.pageNum; i++) {
+            if (1==i) {
+                sHtml += '<p tgpOrder="'+i+'" class="tgp_current">' + i + '</p>';
+            }else {
+                sHtml += '<p tgpOrder="' + i +'">' + i + '</p>';
+            }
         };
-        sHtml += '<div>下一页</div>';
-        this.dom.pagerWrap = sHtml ;
+        sHtml += '<div tgpType="btnNext">下一页</div>';
+        wrap.innerHTML = sHtml ;
+
+        this.dom = {
+            btnPrev: wrap.getElementsByTagName('div')[0]
+            ,btnNext: wrap.getElementsByTagName('div')[1]
+            ,btn: wrap.getElementsByTagName('div')
+            ,num: wrap.getElementsByTagName('p')
+        };
+        this.dom.lastNum = this.dom.num[0];
     }
 
     ,numClick: function () {
@@ -42,8 +46,8 @@ TJ.pager.prototype = {
         for (var i = 0, k = null; k = this.dom.num[i] ; i++ ) {
             k.v = Number(k.innerHTML) ;
             k.onclick = function () {
-                TJ(me.dom.lastNum).removeClass('mod_pager_cur');
-                TJ(this).addClass('mod_pager_cur');
+                TJ(me.dom.lastNum).removeClass('tgp_current');
+                TJ(this).addClass('tgp_current');
                 me.statusChange(this.v);
                 me.render((this.v -1) * me.perPage);
                 me.dom.lastNum = this;
@@ -55,19 +59,19 @@ TJ.pager.prototype = {
         var me = this;
         for (var i = 0, k = null; k = this.dom.btn[i] ; i++ ) {
             k.onclick = function () {
-                if (TJ(this).hasClass('mod_pager_dis')) {
+                if (TJ(this).hasClass('tgp_disabled')) {
                     return  ;
                 }
                 var lastOrder = Number(me.dom.lastNum.innerHTML),
                     newOrder = 0;
-                if (this.getAttribute('dataType') == 'btnPrev') {
+                if (this.getAttribute('tgpType') == 'btnPrev') {
                     newOrder = lastOrder - 1;
-                }else if (this.getAttribute('dataType') == 'btnNext'){
+                }else if (this.getAttribute('tgpType') == 'btnNext'){
                     newOrder = lastOrder + 1;
                 }
 
-                TJ(me.dom.lastNum).removeClass('mod_pager_cur');
-                TJ(me.dom.num[newOrder-1]).addClass('mod_pager_cur');
+                TJ(me.dom.lastNum).removeClass('tgp_current');
+                TJ(me.dom.num[newOrder-1]).addClass('tgp_current');
                 me.render((newOrder -1) * me.perPage);
                 me.statusChange(newOrder);
 
@@ -77,18 +81,19 @@ TJ.pager.prototype = {
     }
 
     ,statusChange: function (v) {
-        TJ(this.dom.btn).removeClass('mod_pager_dis');
+        TJ(this.dom.btn).removeClass('tgp_disabled');
 
         if (v == 1) {
-            TJ(this.dom.btnPrev).addClass('mod_pager_dis');
+            TJ(this.dom.btnPrev).addClass('tgp_disabled');
         }else if (this.pageNum == v){
-            TJ(this.dom.btnNext).addClass('mod_pager_dis');
+            TJ(this.dom.btnNext).addClass('tgp_disabled');
         }
     }
 };
 
-/* =====topest data ===== */
-var topestData = {
+/* =====custom data ===== */
+var custom = {};
+custom.data = {
     "data": [
         ["1", "奉义中郎将五级", "脆脆", "35974"],
         ["2", "武卫中郎将五级", "骑砍、小双", "27545"],
@@ -169,59 +174,36 @@ var topestData = {
         ["77", "典军校尉五级", "狼兵メ猪头", "8630"],
         ["78", "典军校尉四级", "铁骑丶斌斌", "8507"],
         ["79", "典军校尉一级", "君临城主", "8436"],
-        ["80", "典军校尉二级", "卷毛", "8301"],
-        ["81", "武卫中郎将三级", "Homura", "8261"],
-        ["82", "典军校尉二级", "骑砍_SYS", "8258"],
-        ["83", "典军校尉五级", "冷漠de眼神", "8220"],
-        ["84", "典军校尉一级", "守恒K", "8143"],
-        ["85", "典军校尉五级", "血盟_大嫖客", "8112"],
-        ["86", "武卫中郎将一级", "珈蓝丶卧虫", "8110"],
-        ["87", "中军校尉五级", "骑砍_韩信", "8025"],
-        ["88", "典军校尉四级", "马超", "8022"],
-        ["89", "武卫中郎将五级", "迦楼西", "7972"],
-        ["90", "典军校尉四级", "曹曹", "7920"],
-        ["91", "典军校尉五级", "慕天狼丶", "7905"],
-        ["92", "典军校尉一级", "绯...", "7755"],
-        ["93", "典军校尉五级", "HERO尛燚", "7733"],
-        ["94", "典军校尉四级", "龙吟丶马超", "7696"],
-        ["95", "典军校尉二级", " 铁骑家园", "7600"],
-        ["96", "典军校尉一级", "See", "7563"],
-        ["97", "典军校尉五级", "丶清剑", "7519"],
-        ["98", "典军校尉二级", " 笑忘五湖风", "7479"],
-        ["99", "典军校尉一级", "Mrw丶唯枫", "7451"],
-        ["100", "武卫中郎将四级", "曇天", "7447"]
+        ["80", "典军校尉二级", "卷毛", "8301"]
     ]
 };
-var topest = {};
-topest.render = function (elParent, order, len) {
-    var d = topestData.data,
-        sHtml = '';
-
-    for (var i = order || 0 , count = 1; i < len + order; i++ ) {
-        var bgCls = '',
-            colorCls = '';
-        if (count % 2 != 0) {
-            bgCls = 'bg_odd';
-        }
-        if (count<4) {
-            colorCls = 'co_red'+count;
-        }
-        count++;
-        sHtml += '<li class="' + bgCls + ' ' + colorCls + '"><p class="c1">' + this.fixNum(d[i][0], 3) + '</p><p>' + d[i][1] + '</p><p>' + d[i][2] + '</p><p>' + d[i][3] + '</p></li>';
-    }
-    elParent.innerHTML = sHtml ;
-};
-topest.fixNum = function (v, len) {
+custom.fixNum = function (v, len) {
     return len > v.length ? (new Array(len-v.length + 1)).join('0')+v : v ;
 };
 
-var topPagerArg = {
-    wrapId: 'xPager'
-    ,totalCount: topestData.data.length
+custom.render = function (start, len) {
+    var d = this.data.data,
+        sHtml = '';
+
+    for (var i = start , count = 1; i < len + start; i++ ) {
+        var bgCls = '',
+            colorCls = '';
+        if (count % 2 != 0) { bgCls = 'bg_odd'; }
+        if (count<4) { colorCls = 'co_red'+count; }
+        count++;
+
+        sHtml += '<li class="' + bgCls + ' ' + colorCls + '"><p class="c1">' + this.fixNum(d[i][0], 3) + '</p><p>' + d[i][1] + '</p><p>' + d[i][2] + '</p><p>' + d[i][3] + '</p></li>';
+    }
+    document.getElementById('xList').innerHTML = sHtml ;
+};
+
+custom.config = {
+    pagerWrap: document.getElementById('tgPager')
+    ,totalCount: custom.data.data.length
     ,perPage: 10
     ,render:function (start) {
-        topest.render(TJ('#xList')[0], start, topPagerArg.perPage );
+        custom.render(start, custom.config.perPage );
     }
 };
-// init topest pager
-var pTop = new TJ.pager(topPagerArg);
+// init 
+var customPager = new TGPager(custom.config);
